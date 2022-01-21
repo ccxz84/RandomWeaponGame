cd30d57a2af3b3fdc49fd94495e1c1da6e009b61
ccxz84 <ccxz84@naver.com>
2022-01-22 00:47:30 +0900

    initial setting

---
+++ Source/RandomWeaponGameEditor.Target.cs
@@ -0,0 +1,14 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+using UnrealBuildTool;
+using System.Collections.Generic;
+
+public class RandomWeaponGameEditorTarget : TargetRules
+{
+	public RandomWeaponGameEditorTarget(TargetInfo Target) : base(Target)
+	{
+		Type = TargetType.Editor;
+		DefaultBuildSettings = BuildSettingsVersion.V2;
+		ExtraModuleNames.Add("RandomWeaponGame");
+	}
+}

f32aacc11702ffc8642b1cc91fb9bd3d5c245abf
ccxz84 <ccxz84@naver.com>
2022-02-17 01:25:03 +0900

    [feat] 리신 모델 추가
    - 리신 모델 및 애니메이션 추가

---
+++ Source/RandomWeaponGame/Public/testplayercontroller.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/PlayerController.h"
+#include "testplayercontroller.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class RANDOMWEAPONGAME_API Atestplayercontroller : public APlayerController
+{
+	GENERATED_BODY()
+	
+};

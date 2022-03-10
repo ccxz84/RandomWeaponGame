c83f83c3016a8ab970664eaa6e128829d1cd589f
ccxz84 <ccxz84@naver.com>
2022-03-10 11:12:20 +0900

    [chore] 형상 꼬임 형상 꼬임에 따른 재커밋

--- Source/RandomWeaponGame/RandomWeaponGameGameMode.cpp
+++ Source/RandomWeaponGame/RandomWeaponGameGameMode.cpp
@@ -1,7 +1,7 @@
 // Copyright Epic Games, Inc. All Rights Reserved.
 
 #include "RandomWeaponGameGameMode.h"
-#include "RandomWeaponGameCharacter.h"
+#include "Character/PlayerCharacter.h"
 #include "UObject/ConstructorHelpers.h"
 
 ARandomWeaponGameGameMode::ARandomWeaponGameGameMode()
@@ -10,6 +10,7 @@ ARandomWeaponGameGameMode::ARandomWeaponGameGameMode()
 	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnBPClass(TEXT("/Game/ThirdPersonCPP/Blueprints/ThirdPersonCharacter"));
 	if (PlayerPawnBPClass.Class != NULL)
 	{
+		// UE_LOG(LogTemp, Log, TEXT("%s"), *PlayerPawnBPClass.Class.Get()->GetName());
 		DefaultPawnClass = PlayerPawnBPClass.Class;
 	}
 }
